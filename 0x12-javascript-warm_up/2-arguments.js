0a66322babd1bd3b57b740a75a12deb86ef4075d
moifore <moiforejallah@gmail.com>
2023-03-13 07:46:52 -0700

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-if (process.argv.length === 2){
-	console.log('No argument');
-} else if (process.argv.length === 3) {
-	console.log('Argument found');
-} else {
-	console.log('Argument found');
-}
+// prints a message depending of the number of arguments passed
+
+if (process.argv.length <= 2) console.log('No argument');
+else if (process.argv.length === 3) console.log('Argument found');
+else console.log('Arguments found');
