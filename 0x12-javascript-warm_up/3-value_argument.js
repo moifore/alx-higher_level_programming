0b6dcb4e40117cfc9943788d99648f41f3da1465
moifore <moiforejallah@gmail.com>
2023-03-13 08:10:32 -0700

    Value argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 //Script prints the first argument passed to it
 
-if (!process.argv[2] === undefined ) console.log ('No argument');
+if (!process.argv[2] === 1 ) console.log ('No argument');
 else console.log(process.argv[2]);
