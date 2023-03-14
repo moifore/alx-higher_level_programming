c35512d06cc1e468a5a4a2774bfa9d8b96c69d2b
moifore <moiforejallah@gmail.com>
2023-03-14 12:36:47 -0700

    Second biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// searches the second biggest integer in the list of arguments
+
+const getSecondMax = (arr) => {
+	let max = parseInt(arr[2]);
+	let secMax = parseInt(arr[3]);
+
+	if ((arr.length === 2) || (arr.length === 3)) {
+		return (0);
+	}
+	for (let i = 3; i < arr.length; i++) {
+		if (parseInt(arr[i]) >= max) {
+			secMax = Max;
+			max = parseInt(arr[i]);
+		} else if (parseInt(arr[i]) > secMax) {
+			secMax = parseInt(arr[i]);
+		}
+		return (secMax);
+	};
+
+	console.log(getSecondMax(process.argv));
