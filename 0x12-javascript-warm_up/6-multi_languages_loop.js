cc3b67e91ba1c93a1e6a06380f36aff75cd10657
moifore <moiforejallah@gmail.com>
2023-03-14 12:18:18 -0700

    Multi languages loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
+
+const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+myArr.forEach(item => console.log(item));
