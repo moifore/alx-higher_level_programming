344494717929e39a6714927516a7d3ab1779c764
moifore <moiforejallah@gmail.com>
2023-03-19 02:51:27 -0700

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,50 +1,12 @@
 #!/usr/bin/python3
 
-""" This module lists all states from a database
-It takes the following arguments:
-    mysql username
-    mysql password
-    database name
-"""
+# Lists all states from btn_0e_0_usa database.
 
+import sys
 import MySQLdb
-from sys import argv as argv
 
-def db_connect(username, password, database, host="localhost", port=3306):
-    connection = None
-    connection = MySQLdb.connect(user=username, passwd=pasword, db=database)
-    return connection
-
-def db_cursor(connection):
-    cursor = None
-    cursor = connnection.cursor()
-    return cursor
-
-def db_execute(cursor, command):
-    rows = None
-    rows = cursor.execute(command)
-    return rows
-
-def db_print_data(row):
-    data = None
-    data = rows.fetchall()
-    for rows.fetchall()
-    for row in data:
-        print(f"{row}")
-    return data
-
-def db_clean_up(cursor, connection):
-    cursor.close()
-    connection.close()
-
-if __name__ == '__main__':
-    if len(argv) < 4:
-        print('Usage: argv[0] <password> <database>')
-        exit()
-    command = 'SELECT * FROM states ORDER BY id ASC'
-    connection = db_connect(argv[1], argv[2], argv[3])
-    cursor = db_cursor(connection)
-    rows = db_execute(cursor, command)
-    data = db_print_data(cursor)
-    db_clean_up(cursor, connection)
-    
+if __name__=="__main__":
+    connect_db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], connect_db=sys.argv[3])
+    c = connect_db.cursor()
+    c.execute("SELECT * FROM 'states' ORDER BY 'states.id' ASC ")
+    [print(state) for state in c.fetchall()]
